c7787956765acc39bad7d81538cc0f6a667d6f40
serhii.chyshkala <serhii.chyshkala@cengage.com>
2024-01-17 22:13:30 +0200

    Section 19(1)

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
 } from '@angular/common/http';
-import { Observable, delay, finalize } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Observable, delay, finalize, identity } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { BusyService } from '../_services/busy.service';
 
 @Injectable()
@@ -16,7 +17,7 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busyService.busy();
     return next.handle(request).pipe(
-      delay(1000),
+      (environment.production ? identity : delay(1000)),
       finalize(() => {
         this.busyService.idle()
       })
